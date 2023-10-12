8f814e8cc356126a93c451028dc882419b64784a
Replit user <>
2023-10-12 20:26:47 +0000

    new try

--- heap.c
+++ heap.c
@@ -56,7 +56,17 @@ void heap_push(Heap* pq, void* data, int priority){
 
 
 void heap_pop(Heap* pq){
+  if(pq==NULL || pq->size==0){
+    printf("Error al crear el monticulo\n");
+    return
+  }
 
+  pq->heapArray[0]=pq->heapArray[pq->size-1];
+  pq->size--;
+
+  int indice=0;
+  int ultimo = pq->size-1;
+  
 }
 
 Heap* createHeap(){
