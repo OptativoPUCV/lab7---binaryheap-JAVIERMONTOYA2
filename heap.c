5632541651fdc790f01eb2611b864d79eb487b89
Replit user <>
2023-10-12 18:32:15 +0000

    new try

--- heap.c
+++ heap.c
@@ -40,7 +40,7 @@ Heap* createHeap(){
   }
   monticulo->size=0;
   monticulo->capac=3;
-  monticulo->heapArray=(int*)malloc(monticulo->capac*sizeof(int));
+  monticulo->heapArray=(int*)malloc(heap->capac*sizeof(int));
   if(monticulo->heapArray==NULL){
     printf("Error al crear el monticulo\n");
     return NULL;
